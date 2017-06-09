cf70a8d4b48767f49aa5509c8042d618122eb251
bglasner1 <bglasner319@gmail.com>
2017-06-08 18:35:46 -0700

    runs 4,5,6 data

--- Loop_Run_6.m
+++ Loop_Run_6.m
@@ -2,7 +2,7 @@
 % See comments in MinTime_RHC_VaryComm_2D_CVX for additional detail
 fprintf('Starting Run 6\r\n');
 N_agents = 20;%10,11:3;20,25,30]; % Range of number of agents to simulate
-CommRange = [0,2,10,100,1,3,15,50,5]; % Communication range - 0 is no comunication, 20 is full
+CommRange = [15,50,5]; % Communication range - 0 is no comunication, 20 is full
 ExecHorz = 1; % Number of steps to take before replanning.
 PlanHorz = 10; % Number of steps the algorithm plans per iteration
 
